a22762d6cfb2af61486680c188385a6f5c593848
Apoorva Pandey <apoorvapandey365@gmail.com>
2017-08-31 00:43:45 +0530

    Creates profile

--- aeon/settings.py
+++ aeon/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_framework_docs',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,9 @@ WSGI_APPLICATION = 'aeon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'aeon',
+        'ATOMIC_REQUESTS': True,
     }
 }
 
